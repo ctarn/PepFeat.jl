76be933b2dc10048a87220e6f64f41bb835abeb4
Tarn Yeong Ching <i@ctarn.io>
2023-03-11 21:17:27 +0800

    fix entry point of PepFeatAlign

--- src/PepFeat.jl
+++ src/PepFeat.jl
@@ -4,6 +4,6 @@ include("PepFeatDetect.jl")
 include("PepFeatAlign.jl")
 
 main_PepFeatDetect()::Cint = PepFeatDetect.julia_main()
-main_PepFeatAlign()::Cint = PepFeatDetect.julia_main()
+main_PepFeatAlign()::Cint = PepFeatAlign.julia_main()
 
 end
