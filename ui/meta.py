a95a15d1b0d4803bd1b4f245472afa475ba09339
Tarn Yeong Ching <i@ctarn.io>
2023-04-20 21:10:11 +0800

    release 1.1.2

--- ui/meta.py
+++ ui/meta.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 name = "PepFeat"
-version = "1.1.1"
+version = "1.1.2"
 author = "Tarn Yeong Ching"
 url = f"http://{name.lower()}.ctarn.io"
 server = f"http://api.ctarn.io/{name}/{version}"
