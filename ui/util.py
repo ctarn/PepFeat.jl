ea2e892cfa5f4e348ae1b213b6479c8f899da754
Tarn Yeong Ching <i@ctarn.io>
2023-04-27 20:24:02 +0800

    ui: sync `util.py`

--- ui/util.py
+++ ui/util.py
@@ -4,8 +4,11 @@ import os
 import platform
 import subprocess
 import sys
+import tkinter as tk
 from tkinter import ttk
 from urllib import request
+
+import meta
 
 # OS
 is_linux = platform.system() == "Linux"
@@ -87,3 +90,24 @@ def show_headline(url, frame, column=0, row=0, columnspan=1):
             ttk.Label(frame, text=text).grid(column=column, row=row, columnspan=columnspan)
     except:
         pass
+
+def center_window(win):
+    x = int((win.winfo_screenwidth() - win.winfo_width()) / 2)
+    y = int((win.winfo_screenheight() - win.winfo_height()) / 2)
+    win.geometry(f"+{x}+{y}")
+
+sty_entry = {"sticky": "WE", "pady": 1}
+sty_button = {"sticky": "WE", "padx": 4, "pady": 1}
+
+def create_window(pos):
+    win = tk.Tk()
+    win.title(meta.name)
+    win.iconphoto(True, tk.PhotoImage(file=get_content(f"{meta.name}.png", shared=True)))
+    win.resizable(False, False)
+    win.overrideredirect(True)
+    center_window(win)
+    def on_click(e):
+        pos[0], pos[1] = e.x, e.y
+    win.bind("<Button-1>", on_click)
+    win.bind("<B1-Motion>", lambda e: win.geometry(f"+{e.x_root - pos[0]}+{e.y_root - pos[1]}"))
+    return win
